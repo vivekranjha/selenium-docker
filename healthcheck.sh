f3c66e20e877417e746e8e4f20113be88803a9e1
rubal choudhary <you@example.com>
2021-06-04 00:50:59 +0530

    edit healthchk

--- healthcheck.sh
+++ healthcheck.sh
@@ -6,9 +6,7 @@
 
 echo "Checking if hub is ready - $HUB_HOST"
 
-while [ "$( curl -s http://$HUB_HOST:4444/wd/hub/status | jq -r .value.ready )" != "true" ]
-do
-	sleep 1
+while [[ "$( curl -s http://$HUB_HOST:4444/wd/hub/status | jq -r .value.ready )" != "true" ]]; do sleep 1
 done
 
 # start the java command
